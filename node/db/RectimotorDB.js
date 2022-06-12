15f30368e039b7f76796b0d82de6ffa9749394a7
Dsotofuya <dsotofuya@gmail.com>
2022-06-12 10:10:39 -0500

    Creación de paquetes y archivos iniciales para la parte del servidor.

--- node/db/RectimotorDB.js
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from "sequelize";
-const db = new Sequelize('dsotofuya_trabajodecampo_rectimotor', 'dsotofuya', 'robot2300', {
-    host: 'mysql-dsotofuya.alwaysdata.net',
-    dialect: 'mysql'
-})
-
-export default db; 
