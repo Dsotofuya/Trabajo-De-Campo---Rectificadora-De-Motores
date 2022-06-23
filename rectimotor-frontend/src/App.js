acd012c15d1aa71432bcb16259a04fd5bce25b7e
NicolasRojas64 <nicolas.rojas04@uptc.edu.co>
2022-06-23 04:38:51 -0500

    Capturar y almacenar búsqueda en formulario

--- rectimotor-frontend/src/App.js
+++ rectimotor-frontend/src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [search, setSearch] = useState('');
   return (
     <div className="App">
       <h1>Consulte el estado de su motor</h1>
       <div className='contenedor-principal'>
-        <form>
-          <input className='input' type='text' name='Buscar'></input>
+        <form onSubmit={ev => {ev.preventDefault(); setSearch(ev.target.search.value)}}>
+          <input className='input' type='text' name='search'></input>
           <button className='buton' variant='primary' type='submit'>Buscar</button>
         </form>
+
+        <div className='resultados-busqueda'>
+          <p>Resultados para: {search}</p>
+        </div>
       </div>
     </div>
   );
