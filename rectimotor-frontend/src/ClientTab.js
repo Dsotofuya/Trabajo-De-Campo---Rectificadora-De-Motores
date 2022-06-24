3f648b22e73e6f4d906be00cc9adca4845592194
SergioV19 <sergiovar19@gmail.com>
2022-06-24 11:10:26 -0500

    bootstrap funcional

--- rectimotor-frontend/src/ClientTab.js
+++ rectimotor-frontend/src/ClientTab.js
@@ -5,10 +5,21 @@ const ClientTab = ({ nameMotor, nameWorkshop, ownerName, ownerId, engineStatus }
     return (
         <div>
             <div className="resultados-busqueda">
+                <div className="row">
+
+                    <div className="col-sm">
                         <p>Nombre del motor: &nbsp; {nameMotor} &nbsp;</p>
+                    </div>
+                    <div className="col-sm">
                         <p>Nombre del taller: &nbsp; {nameWorkshop} &nbsp;</p>
+                    </div>
+                    <div className="col-sm">
                         <p>Nombre del propietario: &nbsp; {ownerName} &nbsp;</p>
+                    </div>
+                    <div className="col-sm">
                         <p>Cédula del propietario: &nbsp; {ownerId} &nbsp;</p>
+                    </div>
+                </div>
             </div>
             <p>{engineStatus}</p>
         </div>
