a13890b7ac7b5a1e8ad27eb98923d8cd2f4d9ed2
SergioV19 <sergiovar19@gmail.com>
2022-07-21 15:07:54 -0500

    arreglo de PersonModal

--- rectimotor-frontend/src/componentes/PersonModal.js
+++ rectimotor-frontend/src/componentes/PersonModal.js
@@ -4,79 +4,74 @@ import MenuButton from "./MenuButton";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-export default class PersonModal extends Component {
-    render() {
-        const { children, toggle, active } = this.props
+function PersonModal() {
+    const { children, toggle, active } = this.props
 
 
-        return (
-            <Portal>
-                {active && (
-                <div className="container">
-                    <h2 className="text-center">Registro de Cliente</h2>
-                    <br />
+    return (
+        <div className="container">
+            <h2 className="text-center">Registro de Cliente</h2>
+            <br />
 
-                    <div className="container">
+            <div className="container">
 
-                        <div className="row">
+                <div className="row">
 
-                            <div className="col">
-                                <label>Nombre de la persona: </label>
-                            </div>
+                    <div className="col">
+                        <label>Nombre de la persona: </label>
+                    </div>
 
-                            <div className="col">
-                                <input type="text" placeholder="Nombre" />
-                            </div>
-
-                        </div>
-
-                        <div className="row">
-
-                            <div className="col">
-                                <label>Cedula de la persona: </label>
-                            </div>
-
-                            <div className="col">
-                                <input type="text" placeholder="Cedula" />
-                            </div>
-
-                        </div>
-
-                        <div className="row">
-                            <div className="col">
-                                <label>Teléfono</label>
-                            </div>
-
-                            <div className="col">
-                                <input type="tel" placeholder="teléfono" />
-                            </div>
-                        </div>
-
-
-                        <br />
-
-                        <Link to='/menu' className="text-decoration-none">
-                            <div className="row">
-                                <button className="btn btn-success">
-                                    <h3>Confirmar</h3>
-                                </button>
-                            </div>
-                        </Link>
-                        {/* <Link to='/menu' className="text-decoration-none"> */}
-                            <div className="row">
-                                <button className="btn btn-danger" onClick={toggle}>
-                                    <h3 className="text-decoration-none">Descartar</h3>
-                                </button>
-                            </div>
-
-                            <div>{children}</div>
-                        {/* </Link> */}
-
+                    <div className="col">
+                        <input type="text" placeholder="Nombre" />
                     </div>
 
                 </div>
-                )}
-            </Portal>
-        );
-    }
+
+                <div className="row">
+
+                    <div className="col">
+                        <label>Cedula de la persona: </label>
+                    </div>
+
+                    <div className="col">
+                        <input type="text" placeholder="Cedula" />
+                    </div>
+
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <label>Teléfono</label>
+                    </div>
+
+                    <div className="col">
+                        <input type="tel" placeholder="teléfono" />
+                    </div>
+                </div>
+
+
+                <br />
+
+                <Link to='/menu' className="text-decoration-none">
+                    <div className="row">
+                        <button className="btn btn-success">
+                            <h3>Confirmar</h3>
+                        </button>
+                    </div>
+                </Link>
+                {/* <Link to='/menu' className="text-decoration-none"> */}
+                <div className="row">
+                    <button className="btn btn-danger" onClick={toggle}>
+                        <h3 className="text-decoration-none">Descartar</h3>
+                    </button>
+                </div>
+
+                <div>{children}</div>
+                {/* </Link> */}
+
+            </div>
+
+        </div>
+    );
 }
+
