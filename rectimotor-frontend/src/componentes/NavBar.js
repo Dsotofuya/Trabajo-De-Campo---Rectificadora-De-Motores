3005b37f8501b07d8a23f84c333ba68c890082dc
NicolasRojas64 <nicolas.rojas04@uptc.edu.co>
2022-08-01 09:58:16 -0500

    Corrección react icons

--- rectimotor-frontend/src/componentes/NavBar.js
+++ rectimotor-frontend/src/componentes/NavBar.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import {Link} from 'react-router-dom';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
+//import * as FaIcons from 'react-icons/fa';
+//import * as AiIcons from 'react-icons/ai';
+import {FaBars} from 'react-icons/fa';
+import {AiOutlineClose} from 'react-icons/ai';
 import { NavBarContent } from "./NavBarContent";
 import '../estilos/navBar.css'
 
@@ -13,14 +15,14 @@ const NavBar = () => {
     <>
     <div className="navbar">
         <Link to="#" className="bars-button">
-            <FaIcons.FaBars onClick={showSideBar}/>
+            <FaBars onClick={showSideBar}/>
         </Link>
     </div>
     <nav className={sideBar ? 'nav-menu active' : 'nav-menu'}>
         <ul className="nav-elements" onClick={showSideBar}>
             <li className="nav-element">
                 <Link to='#' className="bars-button">
-                    <AiIcons.AiOutlineClose/>
+                    <AiOutlineClose/>
                 </Link>
             </li>
             {NavBarContent.map((item, index) => {
