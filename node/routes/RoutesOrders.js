7a1e873aa972aa21005ed6ee75ad532d32cb3010
Dsotofuya <dsotofuya@gmail.com>
2022-08-05 11:50:29 -0500

    End point obtener orden por id

--- node/routes/RoutesOrders.js
+++ node/routes/RoutesOrders.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createOrder, deleteOrder, getAllOrders, getOrderId, getReportsDetailsById, getOrderByCC, updateOrder } from '../controllers/RectimotorController.js';
+import { createOrder, deleteOrder, getAllOrders, getOrderId, getReportsDetailsById, getOrderByCC, updateOrder, getOrderById } from '../controllers/RectimotorController.js';
 const router = express.Router();
 
 //Rutas De Ordenes
 router.get('/', getAllOrders)
 router.get('/count', getOrderId)
 router.get('/:cc_persona', getOrderByCC)
+router.get('/get/:id_orden', getOrderById)
 
 router.post('/', createOrder)
 router.put('/:id_orden', updateOrder)
