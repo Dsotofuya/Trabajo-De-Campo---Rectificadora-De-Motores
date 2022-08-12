f91b871e8e916110064911599b76da9c0f629922
NicolasRojas64 <nicolas.rojas04@uptc.edu.co>
2022-08-11 22:21:46 -0500

    Whatssapp con menu

--- rectimotor-frontend/src/componentes/Whatssapp.js
+++ rectimotor-frontend/src/componentes/Whatssapp.js
@@ -1,8 +1,12 @@
 import React from "react";
-
+import NavBar from "./NavBar";
+import '../estilos/whatsapp.css'
 function Whatsapp(){
     return(
         <div>
+            <div className="navbar">
+                <NavBar />
+            </div>
             <h1>Wasap</h1>
         </div>
     );
