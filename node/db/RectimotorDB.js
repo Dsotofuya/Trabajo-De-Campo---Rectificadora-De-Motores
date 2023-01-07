13e326f3e7ddbb699248994b47dcd1405272129d
Daniel Soto Fuya <31628414+Dsotofuya@users.noreply.github.com>
2023-01-06 19:01:09 -0500

    Update RectimotorDB.js

--- node/db/RectimotorDB.js
+++ node/db/RectimotorDB.js
@@ -10,12 +10,4 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     }
 })
 
-// var seq = new Sequelize('dsotofuya_trabajodecampo_rectimotor', 'dsotofuya', 'robot2300', {
-//     host: 'mysql-dsotofuya.alwaysdata.net',
-//     dialect: 'mysql'
-// });
-// seq.query('show tables').then(function (rows) {
-//     console.log(JSON.stringify(rows));
-// });
-
 export default db;
